50655b230c2c95081bf41bc6aa0e529cca9f853b
Liam Cammpbell <liam22campbell@gmail.com>
2022-08-05 16:12:55 -0700

    Add path to add engineers to machines

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -82,6 +82,26 @@ namespace Factory.Controllers
       return RedirectToAction("Index");
     }
     
+    public ActionResult AddEngineer(int id)
+    {
+      var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
+      return View(thisMachine);
+    }
+    
+    [HttpPost]
+    public ActionResult AddEngineer(Machine machine, int EngineerId)
+    {
+      if (EngineerId != 0)
+      {
+        _db.EngineerMachine.Add(new EngineerMachine() { EngineerId = EngineerId, MachineId = machine.MachineId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
+    }
+    
+    
+    
     
   }
 }
