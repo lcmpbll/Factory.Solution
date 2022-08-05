2f7da8c9182456a36bd8df9b05025f87c8382926
Liam Cammpbell <liam22campbell@gmail.com>
2022-08-05 16:52:23 -0700

    Add viewbags

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Dynamic;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
 
 namespace Factory.controller
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+    
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+    
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.Engineers = _db.Engineers;
+      ViewBag.Machines = _db.Machines;
       return View();
     }
   }
